c17c94b0a86ead1fc2323f1666874b906d2966b8
otsairaze <ssavelevdenchikk@gmail.com>
2024-11-15 21:59:31 +0200

    initial project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,22 @@
 import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
-  modules: ["@nuxt/image", "@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "shadcn-nuxt"],
+  modules: [
+    "@nuxt/image",
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    [
+      "@nuxtjs/google-fonts",
+      {
+        families: {
+          Lato: {
+            wght: [300, 400, 700],
+            ital: [300],
+          },
+        },
+      },
+    ],
+  ],
   shadcn: {
     prefix: "",
     componentDir: "./components/ui",
