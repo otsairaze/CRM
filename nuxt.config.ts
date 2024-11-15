9c75e54c51e0e959e99ae4bcec9650afa994c178
otsairaze <ssavelevdenchikk@gmail.com>
2024-11-15 21:37:57 +0200

    initial project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from "nuxt/config";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  modules: ["@nuxt/image", "@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "shadcn-nuxt"],
+  shadcn: {
+    prefix: "",
+    componentDir: "./components/ui",
+  },
+});
